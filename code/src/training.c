a45d9f8a080480b5df4be3adbe4cc0ae1b628428
emperornintri <emperornintri@gmail.com>
2025-02-10 08:07:19 +0200

    [FIX] No more memory leaks, this run on 270MB image.

--- code/src/training.c
+++ code/src/training.c
@@ -73,6 +73,25 @@ void training(float learning_rate, int batch_size, int epochs, LeNet5 * network,
         & output_gradients
       );
 
+      free(input_2d_1.samples);
+      free(input_2d_2.samples);
+      free(input_2d_3.samples);
+      free(input_2d_4.samples);
+      free(input_2d_5.samples);
+      free(input_1d_1.samples);
+      free(input_1d_2.samples);
+      free(input_1d_3.samples);
+      free(output.samples);
+      free(input_2d_1_gradients.samples);
+      free(input_2d_2_gradients.samples);
+      free(input_2d_3_gradients.samples);
+      free(input_2d_4_gradients.samples);
+      free(input_2d_5_gradients.samples);
+      free(input_1d_1_gradients.samples);
+      free(input_1d_2_gradients.samples);
+      free(input_1d_3_gradients.samples);
+      free(output_gradients.samples);
+
       count_correct = countCorrect(& output, & labels);
       count_correct_global += count_correct;
       loss = crossEntropyLoss(& output, & labels);
@@ -88,36 +107,6 @@ void training(float learning_rate, int batch_size, int epochs, LeNet5 * network,
         printDouble(100 * (double) count_correct / (double) input_2d_1.sample_count);
         print("%.\n");
       }
-      /*
-      print("Predicted Labels:\n[");
-      for (int i = 0; i < 1; i++)
-      {
-        print("[");
-        for (int j = 0; j < output.column_count - 1; j++)
-        {
-          printDouble(output.samples[i * output.column_count + j]);
-          print(", ");
-        }
-        printDouble(output.samples[i * output.column_count + output.column_count - 1]);
-        print("]");
-      }
-
-      float probabilities[10];
-      print("]\n");
-      softmax (10, output.samples, probabilities);
-      print("Softmax:\n[");
-      for (int i = 0; i < 1; i++)
-      {
-        print("[");
-        for (int j = 0; j < output.column_count - 1; j++)
-        {
-          printDouble(probabilities[j]);
-          print(", ");
-        }
-        printDouble(probabilities[output.column_count - 1]);
-        print("]");
-      }
-      print("]\n");*/
     }
     print("\nEpoch ");
     printUnsignedInteger(epoch);
